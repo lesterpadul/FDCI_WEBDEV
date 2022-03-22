6b67e649f614f0a36d0f4c36e9fc0a0a26b86039
Lester Padul <padullester@gmail.com>
2022-03-22 21:33:04 +0800

    master: initial commit

---
+++ html_ex1/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nickey's Hobbys</title>
+</head>
+<body>
+
+    <table border="1" width="600px">
+        <!-- this area is for the banner -->
+        <tr>
+            <td colspan="3">
+                <table>
+                    <tr>
+                        <td>
+                            <img width="100%" src="./20220226_061132.jpeg" alt="">
+                        </td>
+                        <td>
+                            <img width="100%" src="./20220226_061132.jpeg" alt="">
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+
+        <tr>
+            <td>1</td>
+            <td rowspan="2">2</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>3</td>
+        </tr>
+
+
+        <!-- this area is for the items -->
+        <tr>
+            <td bgcolor="red">
+                <div>
+                    <h4>Top Places</h4>
+                    <ol>
+                        <li>Komacchi</li>
+                        <li>Clubholic</li>
+                        <li>Uptown Vibes</li>
+                        <li>Trademark</li>
+                        <li>FDCI</li>
+                    </ol>
+                </div>
+            </td>
+            <td bgcolor="green">
+                <div>
+                    <h4>Top Websites</h4>
+                    <ol>
+                        <li>
+                            <a target="_blank" href="https://www.youtube.com/">Youtube</a>
+                            <a target href=""></a>
+                            a
+                        </li>
+                        <li>
+                            Google
+
+                        </li>
+                        <li>
+                            Yahoo
+
+                        </li>
+                        <li>
+                            Facebook
+                        
+                        </li>
+                        <li>
+                            Nativecamp
+
+                        </li>
+                    </ol>
+                </div>
+            </td>
+            <td bgcolor="blue">
+                <div>
+                    <h4>Top Gurls</h4>
+                    <ol>
+                        <li>Hilary</li>
+                        <li>Mary</li>
+                        <li>Pierce</li>
+                        <li>Jhanile</li>
+                        <li>Louchin</li>
+                    </ol>
+                </div>
+            </td>
+        </tr>
+
+        <!-- this area is for the blog -->
+        <tr>
+            <td colspan="3">
+                <ul>
+                    <li>
+                        <h4>Touring Camotes with Olanggays</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi est temporibus, veritatis cum, corporis, magni neque ex explicabo ullam mollitia voluptatem! Magnam, debitis excepturi. Id, aut dolore! Maxime, debitis quo.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium officiis consequatur accusamus illum rerum, animi delectus cumque natus, laboriosam atque expedita commodi quia quis incidunt tempora possimus, mollitia ex assumenda?
+                        </p>
+                    </li>
+                    <li>
+                        <h4>ASMR Travels</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium officiis consequatur accusamus illum rerum, animi delectus cumque natus, laboriosam atque expedita commodi quia quis incidunt tempora possimus, mollitia ex assumenda?
+                        </p>
+                    </li>
+                </ul>
+            </td>
+        </tr>
+
+        <!-- this is for the footer -->
+        <tr>
+            <td colspan="3">Copyright 2022</td>
+        </tr>
+    </table>
+
+</body>
+</html>
